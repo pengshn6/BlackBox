8ee9d987d24d5e6026c2f430280b1a94158acb4f
BlackBoxing <ClearNameOfWorld149@protonmail.com>
2022-07-27 23:43:41 +0800

    fake device info enhanced

--- Bcore/src/main/cpp/Hook/SystemPropertiesHook.cpp
+++ Bcore/src/main/cpp/Hook/SystemPropertiesHook.cpp
@@ -5,7 +5,6 @@
 #include "SystemPropertiesHook.h"
 #include <IO.h>
 #include <BoxCore.h>
-//#include "UnixFileSystemHook.h"
 #import "JniHook/JniHook.h"
 #include "shadowhook.h"
 #include "Log.h"
@@ -30,6 +29,22 @@ HOOK_JNI(jstring, native_get, JNIEnv *env, jobject obj, jstring key, jstring def
     return orig_native_get(env, obj, key, def);
 }
 
+HOOK_JNI(int, __system_property_get, const char *name, char *value) {
+    SHADOWHOOK_STACK_SCOPE();
+    log_print_debug("calling __system_property_get");
+    if (NULL == name || NULL == value) {
+        return orig___system_property_get(name, value);
+    }
+//    log_print_debug("calling __system_property_get");
+    ALOGD(name, value);
+    auto ret = prop_map.find(name);
+    if (ret != prop_map.end()) {
+        const char *ret_value = ret->second.c_str();
+        strcpy(value, ret_value);
+        return strlen(ret_value);
+    }
+    return orig___system_property_get(name, value);
+}
 
 void SystemPropertiesHook::init(JNIEnv *env) {
     prop_map.insert(map<std::string, std::string>::value_type("ro.product.board", "umi"));
@@ -50,4 +65,6 @@ void SystemPropertiesHook::init(JNIEnv *env) {
                         "native_get", "(Ljava/lang/String;Ljava/lang/String;)Ljava/lang/String;",
                         (void *) new_native_get,
                         (void **) (&orig_native_get), true);
+    shadowhook_hook_sym_name("libc.so", "__system_property_get", (void *) new___system_property_get,
+                             (void **) (&orig___system_property_get));
 }
