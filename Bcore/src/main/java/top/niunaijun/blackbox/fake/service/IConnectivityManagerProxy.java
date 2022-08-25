fe6d63e0d627b23ba50f12eb073c7cd699e3f340
BlackBoxing <ClearNameOfWorld149@protonmail.com>
2022-08-25 21:41:28 +0800

    fix vlc crash bug

--- Bcore/src/main/java/top/niunaijun/blackbox/fake/service/IConnectivityManagerProxy.java
+++ Bcore/src/main/java/top/niunaijun/blackbox/fake/service/IConnectivityManagerProxy.java
@@ -1,10 +1,18 @@
 package top.niunaijun.blackbox.fake.service;
 
 import android.content.Context;
+import android.net.wifi.WifiInfo;
+
+import java.lang.reflect.Method;
 
 import black.android.net.BRIConnectivityManagerStub;
+import black.android.net.wifi.BRWifiInfo;
+import black.android.net.wifi.BRWifiSsid;
 import black.android.os.BRServiceManager;
+import top.niunaijun.blackbox.fake.frameworks.BLocationManager;
 import top.niunaijun.blackbox.fake.hook.BinderInvocationStub;
+import top.niunaijun.blackbox.fake.hook.MethodHook;
+import top.niunaijun.blackbox.fake.hook.ProxyMethod;
 import top.niunaijun.blackbox.fake.hook.ScanClass;
 import top.niunaijun.blackbox.fake.service.base.ValueMethodProxy;
 
@@ -37,12 +45,36 @@ public class IConnectivityManagerProxy extends BinderInvocationStub {
     @Override
     protected void onBindMethod() {
         super.onBindMethod();
-        addMethodHook(new ValueMethodProxy("getAllNetworkInfo", null));
-        addMethodHook(new ValueMethodProxy("getAllNetworks",null));
+//        addMethodHook(new ValueMethodProxy("getAllNetworkInfo", null));
+//        addMethodHook(new ValueMethodProxy("getAllNetworks", null));
     }
 
     @Override
     public boolean isBadEnv() {
         return false;
+    }
+
+    @ProxyMethod("getAllNetworkInfo")
+    public static class GetAllNetworkInfo extends MethodHook {
+
+        @Override
+        protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            if (BLocationManager.isFakeLocationEnable()) {
+                return null;
+            }
+            return method.invoke(who, args);
+        }
+    }
+
+    @ProxyMethod("getAllNetworks")
+    public static class GetAllNetworks extends MethodHook {
+
+        @Override
+        protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            if (BLocationManager.isFakeLocationEnable()) {
+                return null;
+            }
+            return method.invoke(who, args);
+        }
     }
 }
